72d02748534ebeb2b95c86207ac28d9b663863bc
yaswanth chanumolu <121846362+yaswanthch24@users.noreply.github.com>
2024-01-06 00:02:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var moreContent = document.querySelector('.more-content');
+    var readMoreButton = document.getElementById('readMoreBtn');
+
+    var originalText = moreContent.innerHTML;
+
+    function truncateContent() {
+        var truncatedText = originalText.slice(0, 200) + '...';
+        moreContent.innerHTML = truncatedText;
+    }
+
+    truncateContent();
+
+    function checkScreenWidth() {
+        return window.innerWidth < 768;
+    }
+
+    function toggleReadMoreButton() {
+        if (checkScreenWidth()) {
+            readMoreButton.style.display = 'block';
+            truncateContent();
+        } else {
+            readMoreButton.style.display = 'none'; 
+            moreContent.innerHTML = originalText; 
+        }
+    }
+
+    toggleReadMoreButton()
+
+    window.addEventListener('resize', toggleReadMoreButton);
+
+    readMoreButton.addEventListener('click', function () {
+        if (moreContent.innerHTML === originalText) {
+            truncateContent();
+            readMoreButton.textContent = 'Read more';
+        } else {
+            moreContent.innerHTML = originalText;
+            readMoreButton.textContent = 'Read less';
+        }
+    });
+});
